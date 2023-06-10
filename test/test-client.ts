d292472aa62ead735082b9bfc4efef62b41ac9ca
duoertai <duoertai@gmail.com>
2023-06-10 05:39:09 -0700

    move client test to /test

---
+++ test/test-client.ts
@@ -0,0 +1,19 @@
+import { UnregisteredClient } from "../iwf/src/unregistered-client";
+import { UnregisteredWorkflowOptionsBuilder } from "../iwf/src/unregistered-workflow-options";
+
+const clientOptions = {
+    serverUrl: "http://localhost:8801",
+    workerUrl: "http://localhost:8802"
+}
+
+const client = new UnregisteredClient(clientOptions);
+client.startWorkflow(
+    "basic", 
+    "test", 
+    "state1", 
+    {encoding: '', data : 'start'}, 
+    101,
+    UnregisteredWorkflowOptionsBuilder.newBuilder().setCronSchedule("*/1 * * * *").build())
+    .then((workflowRunId) => {
+    console.log(workflowRunId);
+});
